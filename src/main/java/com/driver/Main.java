2e1551a46df1218c22f4568a659a7d780130f69a
Deepesh <deepeshkr25899@gmail.com>
2025-10-08 11:18:38 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+
 public class Main {
-  
+  public static void main(String [] args)
+  {
+     RWOnly obj=new RWOnly();
+     obj.name="Deepesh";
+     System.out.println(name);//java: name has private access in com.driver.RWOnly
+      obj.setName("Deepesh");
+      obj.getName();
+      System.out.println(name);
+  }
 }
